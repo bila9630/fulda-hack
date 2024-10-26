422b01dfb350e06e883c1787ad8af3b5ff2c2f4d
Viet Duc Kieu <viet.duc.kieu@web.de>
2024-10-26 19:45:12 +0200

    upload page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+// import { ModeToggle } from "@/components/dark-mode-toggle";
+
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Welcome to our V2G app</h1>
+        <Link href="/user/search" passHref>
+          <Button>
+            Go to User search
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
